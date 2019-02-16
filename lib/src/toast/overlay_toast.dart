0a6bc4358d356e1a4ce0f379f2579b465495df6c
boyan <yangbinyhbn@gmail.com>
2019-02-16 16:52:39 +0800

    remove line limit for toast content

--- lib/src/toast/overlay_toast.dart
+++ lib/src/toast/overlay_toast.dart
@@ -21,19 +21,20 @@ class _Toast extends StatelessWidget {
     return SafeArea(
       child: DefaultTextStyle(
         style: Theme.of(context).textTheme.body1.copyWith(color: Colors.white),
-        maxLines: 1,
-        child: Align(
-          alignment: Alignment(0, 0.5),
-          child: ClipRRect(
-            borderRadius:
-                BorderRadius.circular(1000 /*keep background round rect */),
-            child: Container(
-              color: Colors.black87,
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 16,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Align(
+            alignment: Alignment(0, 0.5),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                color: Colors.black87,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 16,
+                ),
+                child: content,
               ),
-              child: content,
             ),
           ),
         ),
