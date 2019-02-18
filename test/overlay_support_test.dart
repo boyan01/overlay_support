3df0ab934981b600a03c4e4df8c483c18758a385
boyan <yangbinyhbn@gmail.com>
2019-02-18 13:16:00 +0800

    tester: add functional test for toast and notification

--- test/overlay_support_test.dart
+++ test/overlay_support_test.dart
@@ -1,7 +1,74 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 void main() {
-  test('adds one to input values', () {
-    expect(1 + 1, 3);
+  setUp(() {
+    kNotificationSlideDuration = Duration.zero;
+    kNotificationDuration = const Duration(milliseconds: 20);
   });
+
+  group('toast', () {
+    testWidgets('toast functional test', (tester) async {
+      await tester.pumpWidget(_FakeOverlay(child: Builder(builder: (context) {
+        return FlatButton(
+            onPressed: () {
+              toast(context, 'message');
+            },
+            child: Text('toast'));
+      })));
+      await tester.pump();
+      await tester.tap(find.byType(FlatButton));
+      await tester.pump();
+
+      expect(find.text('message'), findsOneWidget);
+
+      await tester.pump(const Duration(milliseconds: 50));
+      //already hidden
+      expect(find.text('message'), findsNothing);
+    });
+  });
+
+  group('notification', () {
+    testWidgets('notification functional test', (tester) async {
+      await tester.pumpWidget(_FakeOverlay(child: Builder(builder: (context) {
+        return FlatButton(
+            onPressed: () {
+              showSimpleNotification(context, Text('message'));
+            },
+            child: Text('notification'));
+      })));
+      await tester.pump();
+      await tester.tap(find.byType(FlatButton));
+      await tester.pump();
+
+      expect(find.text('message'), findsOneWidget);
+      await tester.pump(const Duration(milliseconds: 50));
+
+      //already hidden
+      expect(find.text('message'), findsNothing);
+    });
+  });
+}
+
+class _FakeOverlay extends StatelessWidget {
+  final Widget child;
+
+  const _FakeOverlay({Key key, @required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+        textDirection: TextDirection.ltr,
+        child: MediaQuery(
+          data: MediaQueryData(size: const Size(400, 800)),
+          child: Overlay(
+            initialEntries: [
+              OverlayEntry(builder: (context) {
+                return child;
+              }),
+            ],
+          ),
+        ));
+  }
 }
