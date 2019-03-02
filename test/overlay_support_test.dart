6ea3d01b1efa2f900c66b4cee5b6b968c694f01f
boyan <yangbinyhbn@gmail.com>
2019-03-02 22:22:49 +0800

    test : test added for notification

--- test/overlay_support_test.dart
+++ test/overlay_support_test.dart
@@ -48,6 +48,30 @@ void main() {
       //already hidden
       expect(find.text('message'), findsNothing);
     });
+
+    testWidgets('notification hide manually', (tester) async {
+      NotificationEntry entry;
+      await tester.pumpWidget(_FakeOverlay(child: Builder(builder: (context) {
+        return FlatButton(
+            onPressed: () {
+              entry = showSimpleNotification(context, Text('message'),
+                  autoDismiss: false);
+            },
+            child: Text('notification'));
+      })));
+      await tester.pump();
+      await tester.tap(find.byType(FlatButton));
+      await tester.pump();
+
+      assert(entry != null, 'entry has been inited by tap event');
+      expect(find.text('message'), findsOneWidget);
+
+      entry.dismiss(animate: false);
+      await tester.pump();
+
+      //already hidden
+      expect(find.text('message'), findsNothing);
+    });
   });
 }
 
